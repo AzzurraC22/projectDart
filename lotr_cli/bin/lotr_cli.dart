8fc907c7ab30f46f44db44facac74649c41d5e9a
Azzurra <cristofaniazzurra22@gmail.com>
2023-02-16 14:57:59 +0100

    primo commit

--- lotr_cli/bin/lotr_cli.dart
+++ lotr_cli/bin/lotr_cli.dart
@@ -1,5 +1,10 @@
+//ESERCIZIO FIBONACCI
+
 import 'package:lotr_cli/lotr_cli.dart' as lotr_cli;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lotr_cli.calculate()}!');
+  final f = lotr_cli.Fibonacci(count: 5);
+  final l = f.calc();
+  print(l);
 }
+
--- lotr_cli/lib/lotr_cli.dart
+++ lotr_cli/lib/lotr_cli.dart
@@ -1,3 +1,22 @@
-int calculate() {
-  return 6 * 7;
+class Fibonacci {
+  int count;
+
+  Fibonacci({
+    required this.count,
+  });
+
+  List<int> calc(){
+    int n1 = 0;
+    int n2 = 1;
+    int n3;
+    List<int> l = [];
+    for (int i = 0; i <= count; i++) {
+      n3 = n1 + n2;
+      l.add(n3);
+      n1 = n2;
+      n2 = n3;
+    }
+
+    return l;
+    }
 }
